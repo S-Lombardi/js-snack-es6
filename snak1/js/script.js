b8039002bd1a38557fd162ee97799fc75b01c071
S-Lombardi <silo.lombardi@gmail.com>
2023-06-01 17:03:51 +0200

    Fine esercizio

--- snak1/js/script.js
+++ snak1/js/script.js
@@ -1,1 +1,31 @@
 "use strict"
+
+const guests =[
+    
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
+
+console.log(guests)
+
+const placeholderList = guests.map((item, index,) => {
+
+    let object ={
+        table_name: "Tavolo Vip",
+        guests_name: item,
+        seat : index +1, 
+    }
+
+    return object
+
+})
+
+console.log(placeholderList)
