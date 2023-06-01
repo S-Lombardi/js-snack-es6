696a75662fa89341e23fa0c89d5844526ae5c102
S-Lombardi <silo.lombardi@gmail.com>
2023-06-01 16:25:57 +0200

    Richiesta feedback

--- snak2/js/script.js
+++ snak2/js/script.js
@@ -1,1 +1,71 @@
 "use strict"
+
+//Creo la lista degli studenti con un array di oggetti
+const students = [
+    { 
+        'id': 213,
+        'name':'Marco della Rovere',
+        'grades': 78,
+    },
+    { 
+        'id': 110,
+        'name':'Paola Cortellessa',
+        'grades': 96,
+    },
+    { 
+        'id': 250,
+        'name':'Andrea Mantegna',
+        'grades': 48,
+    },
+    { 
+        'id': 145,
+        'name':'Gaia Borromini',
+        'grades': 74,
+    },
+    { 
+        'id': 196,
+        'name':'Luigi Grimaldello',
+        'grades': 68,
+    },
+    { 
+        'id': 102,
+        'name':'Piero della Francesca',
+        'grades': 50,
+    },
+    { 
+        'id': 120,
+        'name':'Francesca da Polenta',
+        'grades': 84,
+    },
+];
+
+
+//Creazione nuova lista con i nomi degli studenti in maiuscolo
+const uppercaseStudents = students.map((item) => {
+    return  item.name.toUpperCase() ;
+});
+
+console.log(uppercaseStudents)
+
+//Creazione lista studenti che hanno il voto superiore a 70
+const gradesStudents = students.filter((item) => {
+
+    if(item.grades > 70){
+
+        return item.name
+    }
+
+});
+console.log(gradesStudents)
+
+//Creazione lista studenti che hanno il voto superiore a 70 e id superiore a 120
+const gradesIdStudents = students.filter((item) => {
+
+    if(item.grades > 70  &&  item.id > 120){
+
+        return true
+    }
+    return false
+
+});
+console.log(gradesIdStudents)
